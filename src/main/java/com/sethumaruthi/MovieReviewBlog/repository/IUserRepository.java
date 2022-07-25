d77fb576992229edd23661c3e04dc7bb7421f7d9
maruthi0209 <sethumaruthi93@gmail.com>
2022-07-25 22:36:59 +0530

    Added user, user role and review module

---
+++ src/main/java/com/sethumaruthi/MovieReviewBlog/repository/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.sethumaruthi.MovieReviewBlog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sethumaruthi.MovieReviewBlog.models.ReviewUser;
+
+public interface IUserRepository extends JpaRepository<ReviewUser, Long>{
+
+}
---
+++ src/main/java/com/sethumaruthi/MovieReviewBlog/service/IUserRoleService.java
@@ -0,0 +1,19 @@
+package com.sethumaruthi.MovieReviewBlog.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import com.sethumaruthi.MovieReviewBlog.models.UserRole;
+
+@Component
+public interface IUserRoleService {
+	
+	public ResponseEntity<UserRole> createRole(UserRole role);
+	
+	public ResponseEntity<UserRole> getRole(Long roleId);
+	
+	public ResponseEntity<List<UserRole>> getAllRoles();
+
+}
