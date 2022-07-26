1c7e97c818a1f73d654d7ab7cbfa3ed4be562938
maruthi0209 <sethumaruthi93@gmail.com>
2022-07-26 22:03:15 +0530

    Added budget and box office fields to movie

--- src/main/java/com/sethumaruthi/MovieReviewBlog/models/Movie.java
+++ src/main/java/com/sethumaruthi/MovieReviewBlog/models/Movie.java
@@ -23,6 +23,12 @@ public class Movie {
 	@Column(name = "release_date", nullable = false)
 	private String releaseDate;
 	
+	@Column(name = "budget", nullable = false)
+	private Long budget;
+	
+	@Column(name = "box_office", nullable = false)
+	private Long boxOffice;
+	
 	@ManyToMany
 	private List<Genre> movieGenre;
 	
@@ -38,13 +44,15 @@ public class Movie {
 	public Movie() {
 		super();
 	}
-
-	public Movie(Long movieId, String movieName, String releaseDate, List<Genre> movieGenre, List<Actor> movieCast,
-			Director movieDirector, Studio movieStudio) {
+	
+	public Movie(Long movieId, String movieName, String releaseDate, Long budget, Long boxOffice,
+			List<Genre> movieGenre, List<Actor> movieCast, Director movieDirector, Studio movieStudio) {
 		super();
 		this.movieId = movieId;
 		this.movieName = movieName;
 		this.releaseDate = releaseDate;
+		this.budget = budget;
+		this.boxOffice = boxOffice;
 		this.movieGenre = movieGenre;
 		this.movieCast = movieCast;
 		this.movieDirector = movieDirector;
@@ -54,8 +62,9 @@ public class Movie {
 	@Override
 	public String toString() {
 		return "Movie [movieId=" + movieId + ", movieName=" + movieName + ", releaseDate=" + releaseDate
-				+ ", movieGenre=" + movieGenre + ", movieCast=" + movieCast + ", movieDirector=" + movieDirector
-				+ ", movieStudio=" + movieStudio + "]";
+				+ ", budget=" + budget + ", boxOffice=" + boxOffice + ", movieGenre=" + movieGenre
+				+ ", movieCast=" + movieCast + ", movieDirector=" + movieDirector + ", movieStudio=" + movieStudio
+				+ "]";
 	}
 
 	public Long getMovieId() {
@@ -114,7 +123,20 @@ public class Movie {
 		this.movieStudio = movieStudio;
 	}
 
-	
-	
+	public Long getBudget() {
+		return budget;
+	}
 
+	public void setBudget(Long budget) {
+		this.budget = budget;
+	}
+
+	public Long getBoxOffice() {
+		return boxOffice;
+	}
+
+	public void setBoxOffice(Long boxOffice) {
+		this.boxOffice = boxOffice;
+	}
+	
 }
