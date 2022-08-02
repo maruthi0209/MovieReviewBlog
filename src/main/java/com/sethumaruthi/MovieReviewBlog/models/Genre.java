901b8b28749902c41f99473bfe1e4148d2c46000
maruthi0209 <sethumaruthi93@gmail.com>
2022-08-02 22:20:30 +0530

    clean up code in genre module

--- src/main/java/com/sethumaruthi/MovieReviewBlog/models/Genre.java
+++ src/main/java/com/sethumaruthi/MovieReviewBlog/models/Genre.java
@@ -1,11 +1,8 @@
 package com.sethumaruthi.MovieReviewBlog.models;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,24 +15,20 @@ public class Genre {
 	
 	@Column(name = "genre_description", nullable = false)
 	private String genreDescription;
-	
-//	@ManyToMany
-//	private List<Movie> genreMovies;
 
 	public Genre() {
 		super();
 	}
 
-	public Genre(Long genreId, String genreDescription, List<Movie> genreMovies) {
+	public Genre(Long genreId, String genreDescription) {
 		super();
 		this.genreId = genreId;
 		this.genreDescription = genreDescription;
-//		this.genreMovies = genreMovies;
 	}
 
 	@Override
 	public String toString() {
-		return "Genre [genreId=" + genreId + ", genreDescription=" + genreDescription + ", genreMovies="				+ "]";
+		return "Genre [genreId=" + genreId + ", genreDescription=" + genreDescription + "]";
 	}
 
 	public Long getGenreId() {
@@ -54,12 +47,5 @@ public class Genre {
 		this.genreDescription = genreDescription;
 	}
 
-//	public List<Movie> getGenreMovies() {
-//		return genreMovies;
-//	}
-//
-//	public void setGenreMovies(List<Movie> genreMovies) {
-//		this.genreMovies = genreMovies;
-//	}
 	
 }
